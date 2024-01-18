373d35a52252c18f8e4cf9cc1f030e55559b5f20
jabahum <jabajnan8@gmail.com>
2024-01-18 16:58:48 +0300

    fix file test error

--- packages/esm-patient-registration-app/src/patient-registration/input/custom-input/fingerprint-capture/thumbprint.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/input/custom-input/fingerprint-capture/thumbprint.component.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import styles from './fingerprint-capture.scss';
-import thumb96 from '../../../../assets/thumb-96.png';
+import thumb96 from '../../../../assets/thumb.png';
 
 interface ThumbPrintProps {
   data: string;
@@ -10,7 +11,7 @@ interface ThumbPrintProps {
 
 const ThumbPrint: React.FC<ThumbPrintProps> = ({ data, onClick, isActive }) => (
   <div className={`${styles.centeredContainer} ${isActive ? styles.activeThumb : ''}`} onClick={() => onClick(data)}>
-    <img src={thumb96} alt={'photo'} className={styles.centeredImage} />
+    <img src={thumb96} alt={'thumb'} className={styles.centeredImage} />
     {data && <p className={styles.centeredText}>Scan {data}</p>}
   </div>
 );
